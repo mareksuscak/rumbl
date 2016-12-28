e87902a7a46933c832ddbe05503d8ff332a3e7e7
Marek Suscak <marek.suscak@gmail.com>
2016-12-29 00:48:50 +0100

    Handle disconnects.

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -40,11 +40,18 @@ const Video = {
     })
     
     vidChannel.on('new_annotation', resp => {
+      vidChannel.params.last_seen_id = resp.id
       this.renderAnnotation(msgContainer, resp)
     })
   
     vidChannel.join()
       .receive('ok', ({annotations}) => {
+        const ids = annotations.map(ann => ann.id)
+        
+        if (ids.length > 0) {
+          vidChannel.params.last_seen_id = Math.max(...ids)
+        }
+        
         this.scheduleMessages(msgContainer, annotations)
       })
       .receive('error', reason => console.log('join failed', reason))
